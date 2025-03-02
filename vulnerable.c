0f1bd63ca40aa029a7e5d50686552f7cbb87ae64
Robert Horne <r.j.horne@kent.ac.uk>
2025-03-02 23:28:53 +0000

    Create vulnerable.c

---
+++ vulnerable.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+void insecure_copy(char* input) {
+    char buffer[16];
+    strcpy(buffer, input); 
+}
+
+void unsafe_input() {
+    char credentials[32];
+    printf("Enter admin credentials: ");
+    gets(credentials); 
+}
+
+int main(int argc, char** argv) {
+    if(argc < 2) {
+        printf("Usage: %s <input>\n", argv[0]);
+        return 1;
+    }
+    
+    insecure_copy(argv[1]);
+    unsafe_input();
+    
+    printf("Program completed normally\n");
+    return 0;
+}
